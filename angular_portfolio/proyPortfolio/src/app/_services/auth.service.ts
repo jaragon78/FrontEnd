9fb950d58188446319be5fb5d7de1aafc0519cfb
jaragon78 <juan.ignacio.aragon@gmail.com>
2023-01-15 12:14:47 -0300

    Se actualiza frontend

--- angular_portfolio/proyPortfolio/src/app/_services/auth.service.ts
+++ angular_portfolio/proyPortfolio/src/app/_services/auth.service.ts
@@ -47,7 +47,7 @@ export class AuthService {
   }
 
   logout(): Observable<any> {
-    return this.http.post(AUTH_API + 'signout', { }, httpOptions);
+    return this.http.post(AUTH_API + 'signout', { });//, httpOptions);
   }
 
   get usuarioAutenticado(){
