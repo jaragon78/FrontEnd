691223aabeee56ccc91bed210ba0c2dd7a3f1237
jaragon78 <juan.ignacio.aragon@gmail.com>
2023-01-20 08:45:58 -0300

    Se actualiza frontend

--- angular_portfolio/proyPortfolio/src/app/componentes/posts/posts.component.ts
+++ angular_portfolio/proyPortfolio/src/app/componentes/posts/posts.component.ts
@@ -15,7 +15,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  private apiUrl =  'https://yoprogramo-jaragon78.koyeb.app'; //'http://localhost:8080'; //
+  apiUrl =  'https://yoprogramo-jaragon78.koyeb.app'; //'http://localhost:8080'; //
   filterPost = '';
   posts: any[];
   persona: Persona;
